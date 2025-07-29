dc4dc6847509a207e827e7b34bb048f156cddb28
Scott Sloan <scottsloan@gmail.com>
2025-07-28 21:06:54 -0500

    Impletmented the Add Transaction

--- src/SimpleLedger.WebApplication/Controllers/TransactionController.cs
+++ src/SimpleLedger.WebApplication/Controllers/TransactionController.cs
@@ -4,7 +4,7 @@ using SimpleLedger.WebApplication.Models.Transaction;
 
 namespace SimpleLedger.WebApplication.Controllers
 {
-    public class TransactionController
+    public class TransactionController : Controller
     {
         private ITransactionService _transactionSrv;
 
@@ -13,10 +13,30 @@ namespace SimpleLedger.WebApplication.Controllers
             _transactionSrv = transactionService;
         }
 
+        public async Task<IActionResult> AddTransaction()
+        {
+
+            return PartialView("AddTransactionModal");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(AddTransactionModel model)
+        {
+            _transactionSrv.SaveTransaction(model.ToLedgerTransaction());
+
+
+            return new JsonResult(new
+            {
+                success = true,
+                message = "Transaction Saved successfully."
+            });
+        }
+
         [HttpPost]
         public async Task<IActionResult> Delete(DeleteTransactionModel model)
         {
             _transactionSrv.DeleteTransaction(model.TransactionId);
+
 
             return new JsonResult(new
             {
---
+++ src/SimpleLedger.WebApplication/Models/Transaction/AddTransactionModel.cs
@@ -0,0 +1,26 @@
+﻿using SimpleLedger.Domain.Entities;
+
+namespace SimpleLedger.WebApplication.Models.Transaction
+{
+    public class AddTransactionModel
+    {
+        public string Payee { get; set; }
+        public string Category { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+
+
+
+        public LedgerTransaction ToLedgerTransaction()
+        {
+            return new LedgerTransaction
+            {
+                Payee = Payee,
+                Category = Category,
+                Date = Date,
+                Amount = Amount
+            };
+        }   
+
+    }
+}
