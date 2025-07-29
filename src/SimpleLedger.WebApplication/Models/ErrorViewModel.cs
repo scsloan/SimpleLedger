15b43e65fc401e2d8f5485e21b97c2f0270bb7e4
Scott Sloan <scottsloan@gmail.com>
2025-07-28 19:29:40 -0500

    Initial Commit

---
+++ src/SimpleLedger.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SimpleLedger.WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
